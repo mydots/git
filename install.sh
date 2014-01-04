db5f099633e61330b65b6624b15500b4b984cb36
Franky W. (franklinwahl - Frankys-MacBook-Pro.local) <frankywahl@users.noreply.github.com>
2014-01-03 19:31:07 -0800

    Initial commit

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+KERNEL="$(uname -s)";
+
+PATH_TO_FILE="$(cd `dirname $0` && pwd)";
+
+export		RED="[0;31m"
+export		GREEN="[0;32m"
+export		DEFAULT="[0;39m"
+
+if [[ "$EMAIL_GIT" == "" ]]; then
+  echo "Please enter your email to use with Git" 
+  read EMAIL_GIT
+  echo "export EMAIL_GIT=${EMAIL_GIT}" >> ~/.keys
+fi
+
+cd ${PATH_TO_FILE} && git submodule init && git submodule update 
+
+# Git
+if which git >/dev/null; then
+  rm -rf ~/.gitconfig
+  touch ${PATH_TO_FILE}/gitconfig 
+  ln -s ${PATH_TO_FILE}/gitconfig ~/.gitconfig
+  git config --global user.name "Franky W. (${USER} - ${HOSTNAME})"
+  git config --global user.email "${EMAIL_GIT}"
+  git config --global merge.tool vimdiff
+  git config --global color.ui true
+  git config --global core.editor vim
+  git config --global core.fileMode false
+  git config --global core.excludesfile ${PATH_TO_FILE}/git/gitignore_global
+  git config --global pager.diff ""
+  git config --global diff.external "git_diff_wrapper"
+  git config --global alias.branchall "!git_branch_all"
+  git config --global alias.lg "log --graph --oneline --decorate --all"
+  git config --global alias.s "status"
+  git config --global alias.ci "commit"
+  git config --global alias.co "checkout"
+  git config --global alias.br "branch"
+  git config --global alias.df "diff"
+  git config --global alias.rb "rebase"
+  git config --global color.decorate.remote red
+  git config --global color.decorate.head cyan
+  git config --global color.decorate.branch green
+  git config --global push.default current
+else
+  echo "${RED}Attention: ${DEFAULT} Git not found"
+fi
+
