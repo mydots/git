a257a3a650daf99600c7b92bf04641ee7fcc4e68
Franky W. (franky - localhost) <franky@newrelic.com>
2014-03-07 17:00:50 -0800

    Added git amend to aliases

--- install.sh
+++ install.sh
@@ -39,6 +39,7 @@ if which git >/dev/null; then
   git config --global alias.br "branch"
   git config --global alias.df "diff"
   git config --global alias.rb "rebase"
+  git config --global alias.amend "commit --amend -C HEAD"
   git config --global color.decorate.remote red
   git config --global color.decorate.head cyan
   git config --global color.decorate.branch green
