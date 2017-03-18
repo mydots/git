0e0793ab14759f0fecfee258351ec6a3675cd11c
Franky W <frankywahl@users.noreply.github.com>
2017-03-18 10:14:51 -0700

    git pushf alias

--- install.sh
+++ install.sh
@@ -44,6 +44,7 @@ if which git >/dev/null; then
   git config --global alias.aliases "config --get-regexp alias"
   git config --global alias.wip "!git add -A && git commit -m 'wip'"
   git config --global alias.rwd "!git checkout HEAD~"
+  git config --global alias.pushf "!git push --force-with-lease"
   git config --global help.autocorrect 5 # Wait 50 ms before autocorrecting
   git config --global color.decorate.remote red
   git config --global color.decorate.head cyan
