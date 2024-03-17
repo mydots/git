ded671c87bb349254db0a40886d8412376faff93
Franky W. <frankywahl@users.noreply.github.com>
2024-03-17 15:41:08 +0100

    Configure SOPS diff texts

--- install.sh
+++ install.sh
@@ -60,6 +60,7 @@ if command -v git >/dev/null; then
   git config --global alias.rwd "!git checkout HEAD~"
   git config --global alias.pushf "!git push --force-with-lease"
   git config --global alias.addnw "!sh -c 'git diff -U0 -w --no-color \"\$@\" | git apply --cached --ignore-whitespace --unidiff-zero -'"
+  git config --global diff.sposdiffer.textconv "sops --decrypt"
 
   git config --global pull.rebase true
   for binary in $(ls ${PATH_TO_FILE}/bin); do
